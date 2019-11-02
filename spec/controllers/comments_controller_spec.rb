a18d1867693e57ed303ba902f7bb1b358b260751
johnsonsirv <johnsonsirv@gmail.com>
2019-11-02 16:30:38 +0000

    all unit test pass

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
-
+	it { should use_before_action(:find_comment) }
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+   context "Associations" do
+		it { should belong_to(:user) }
+		it { should belong_to(:post) }
+		it { should have_many(:likes) }
+	end
+	
+	context "validations" do
+		before do
+    	@comment = FactoryBot.create(:comment)
+  	end
+		it 'is invalid when comment content is null or empty' do
+			comment2 = FactoryBot.build(:comment, content: nil)
+			expect(comment2).to_not be_valid
+		end
+
+		it 'is valid when comment content has value' do
+			expect(@comment).to be_valid
+		end
+	end
 end
