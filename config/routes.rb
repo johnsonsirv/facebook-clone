a9b31c89fba6e7817a7f4d683424813c7be30d9f
johnsonsirv <johnsonsirv@gmail.com>
2019-11-13 15:45:57 +0000

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
   resources :posts, only: %i[index create edit update destroy]
   resources :comments, only: %i[create destroy]
   resources :likes, only: %i[create destroy]
+  resources :friendships, only: [:create, :update, :destroy]
+  
+  get '/friends', to: 'friendships#index'
+  get '/friend_requests', to: 'friendships#friend_requests'
 
   unauthenticated do
     as :user do
