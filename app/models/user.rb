6784cbf68039f827ab18c36bf5c5d483ddf9d1b9
johnsonsirv <johnsonsirv@gmail.com>
2019-11-18 16:49:02 +0000

    added ominiauthable to user, update initializer

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  devise :omniauthable, omniauth_providers: %i[facebook]
 
   def fullname
     "#{firstname} #{lastname}"
