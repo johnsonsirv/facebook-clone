bdbbdd609a6e0773d1031ca34f40c0e78e363cb0
johnsonsirv <johnsonsirv@gmail.com>
2019-11-18 18:12:37 +0000

    setup callbacks for omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -50,6 +50,28 @@ class User < ApplicationRecord
               .where(user: self)
               .where(friend: other_user).exists?
   end
+  
+  
+  def self.from_omniauth(auth)
+		where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      names = auth.info.name.split
+      user.firstname = names.first
+      user.lastname = names.last
+      user.email = auth.info.email
+			user.password = Devise.friendly_token[0, 20]
+      user.sex = 'Custom'
+      user.dob = 18.years.ago
+			user.image_link = auth.info.image
+  	end
+	end
+	
+	def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   devise_for :users, path_names: {
     sign_in: 'login', sign_out: 'logout', sign_up: 'signup'
-  }
+  }, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
 
   resources :users, only: %i[index show]
   resources :posts, only: %i[index create edit update destroy]
